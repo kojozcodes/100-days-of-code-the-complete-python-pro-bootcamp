ebcce17eb5d61c4f390dc36716febcc524f3b00d
Hamza Ahmed <kojozcodes@gmail.com>
2024-07-17 22:48:40 +0100

    add a simple function with no parameter and a function with a parameter

--- Day 8/lesson-83.py
+++ Day 8/lesson-83.py
@@ -1,1 +1,16 @@
 #Function with Inputs
+
+#Simple Function
+def greet():
+    print("Hello")
+    print("World")
+    print("Hello World")
+
+greet()
+
+#Function that allows for input
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"{name}, would you like some chocolates?")
+
+greet_with_name("Hamza")
