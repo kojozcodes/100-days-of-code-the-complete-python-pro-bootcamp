6a5f576d8f5e290e3215c1f8b168e3b2df9d6d64
Hamza Ahmed <kojozcodes@gmail.com>
2024-07-17 01:51:57 +0100

    add day 6 folder and file

---
+++ Day 6/lesson-61.py
@@ -0,0 +1,12 @@
+#Built in functions
+print("Hello World")
+len("Hello World")
+
+#Define functions
+def my_function():
+  print("Hello World")
+
+#For code inside a function to work, the function has to be called
+
+#call a function
+my_function()
