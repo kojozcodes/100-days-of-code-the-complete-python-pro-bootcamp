4a995a3e9ca8e33f8bba524958d9a30a0079c0bb
Hamza Ahmed <kojozcodes@gmail.com>
2024-07-17 01:51:31 +0100

    add day 5 folder and files

---
+++ Day 5/lesson-52.py
@@ -0,0 +1,16 @@
+colors = ["red", "blue", "yellow"]
+for color in colors:
+  print(color)
+  print(color + " house")
+
+#for Loop with Range
+for number in range(1, 11, 3): #3 is number step/gap between the numbers
+  print(number)
+
+#add numbers from 1 to 100
+total = 0
+for number in range(1, 101):
+  total += number
+  print(number)
+
+  
