711305a5a3e4d84d6aedc68efa39c9c7ba88f4ec
Hamza Ahmed <kojozcodes@gmail.com>
2024-07-17 23:03:32 +0100

    add a function with more than 1 parameter, and called the funtion with positional and keyword arguments

--- Day 8/lesson-83.py
+++ Day 8/lesson-83.py
@@ -9,8 +9,16 @@ def greet():
 greet()
 
 #Function that allows for input
-def greet_with_name(name):
+def greet_with_name(name): #name is a parameter
     print(f"Hello {name}")
     print(f"{name}, would you like some chocolates?")
 
-greet_with_name("Hamza")
+greet_with_name("Hamza") #"Hamza" is an argument
+
+#Function with more than 1 parameter (Input)
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}?")
+
+# greet_with("Hamza", "Nigeria") #Positional Arguments
+greet_with(name="Hamza", location="Nigeria") #Keyword Arguments
