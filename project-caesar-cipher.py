ea2aa31d7e3df8e23d9747d59d9798d6c2584d78
Hamza Ahmed <kojozcodes@gmail.com>
2024-07-18 01:16:10 +0100

    implement the decrypt function

--- project-caesar-cipher.py
+++ project-caesar-cipher.py
@@ -1,6 +1,6 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
@@ -13,4 +13,12 @@ def encrypt(plain_text, shift_amount):
       encrypted_text += alphabet[new_position]
     print(f"The encoded text is {encrypted_text}")
 
-encrypt(text, shift)
+def decrypt(encrypt_text, shift_back_amount):
+    print()
+    decrypted_text = ""
+    for letter in encrypt_text:
+      position = alphabet.index(letter)
+      new_position = position - shift_back_amount
+      decrypted_text += alphabet[new_position]
+    print(f"The decoded text is {decrypted_text}")
+
