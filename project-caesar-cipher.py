c52b4d28a6ee9edbd8872a64321b3878da0e5d57
Hamza Ahmed <kojozcodes@gmail.com>
2024-07-18 01:00:45 +0100

    implement the encrypt function

---
+++ project-caesar-cipher.py
@@ -0,0 +1,16 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def encrypt(plain_text, shift_amount):
+
+    encrypted_text = ""
+    for letter in plain_text:
+      position = alphabet.index(letter)
+      new_position = position + shift_amount
+      encrypted_text += alphabet[new_position]
+    print(f"The encoded text is {encrypted_text}")
+
+encrypt(text, shift)
