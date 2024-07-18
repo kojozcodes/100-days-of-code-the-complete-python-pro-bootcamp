ecc40bb3dfc193d8b0704e0a0b6ce6c7c9f09916
Hamza Ahmed <kojozcodes@gmail.com>
2024-07-18 16:31:48 +0100

    improve UI and UX, give user option to continue or to stop etc.

--- Day 8/project-caesar-cipher.py
+++ Day 8/project-caesar-cipher.py
@@ -1,10 +1,5 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-#TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar().
 def caesar(cipher_text, shift_amount, cipher_direction):
 
     encrypted_or_decrypted_text = ""
@@ -14,15 +9,36 @@ def caesar(cipher_text, shift_amount, cipher_direction):
     #       shift_amount *= -1
     # new_position = position += shift_amount
     
-    for letter in cipher_text:
-        position = alphabet.index(letter)
+    for char in cipher_text:
 
-        if cipher_direction == "encode":
-            position = position + shift_amount
-        elif cipher_direction == "decode":
-            position = position - shift_amount
-        encrypted_or_decrypted_text += alphabet[position]
+        if char in alphabet:       
+            position = alphabet.index(char)
+            if cipher_direction == "encode":
+                position = position + shift_amount
+            elif cipher_direction == "decode":
+                position = position - shift_amount
+            encrypted_or_decrypted_text += alphabet[position]
+        else:
+            encrypted_or_decrypted_text += char
 
     print(f"The {cipher_direction}d text is {encrypted_or_decrypted_text}")
 
-caesar(cipher_text=text, shift_amount=shift, cipher_direction=direction)
+from art import logo
+print(logo)
+
+should_continue = True
+while should_continue:
+    
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    shift = shift % 26
+    caesar(cipher_text=text, shift_amount=shift, cipher_direction=direction)
+    
+    user_choice = input("Type 'yes' if you would you like to continue or 'no' if you don't want to continue.").lower()
+    if user_choice == "no":
+        should_continue = False
+        print("Goodbye")
+
+
