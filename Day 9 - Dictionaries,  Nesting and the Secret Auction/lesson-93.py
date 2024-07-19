3bfe39802f0e8ab6a2c4bc9966d6c676e8050aa9
Hamza Ahmed <kojozcodes@gmail.com>
2024-07-19 23:52:32 +0100

    add content on nesting dictionaries and Lists

--- Day 9 - Dictionaries,  Nesting and the Secret Auction/lesson-93.py
+++ Day 9 - Dictionaries,  Nesting and the Secret Auction/lesson-93.py
@@ -17,3 +17,38 @@ programming_dictionary = {}
 
 #edit/override an item in a dictionary
 programming_dictionary["Bug"] = "This is how to edit an item in a dictionary"
+
+"---------------------------------------------------------------------------------"
+
+#Nesting--------------------------------------------------------------------------
+
+Capitals = {
+    "France": "Paris",
+    "Nigeria": "Abuja"
+}
+
+#Nesting a List in a dictionary
+travel_log = {
+    "Nigeria": ["Abuja", "Yola", "Kogi"],
+    "Saudi Arabia": ["Madinah", "Makkah", "Jeddah"]
+}
+
+#Nesting dictionaries in a dictionary
+travel_log2 = {
+    "Nigeria": {"cities-visited": ["Abuja", "Yola", "Kogi"], "total_visits": 100},
+    "Saudi Arabia": {"cities-visited": ["Madinah", "Makkah", "Jeddah"],"total_visits": 1} 
+}
+
+#Nesting dictionaries in Lists
+travel_log3 = [
+    {
+        "country": "Nigeria", 
+        "cities-visited": ["Abuja", "Yola", "Kogi"], 
+        "total_visits": 100
+    }, 
+    {
+        "country": "Saudi Arabia", 
+        "cities-visited": ["Madinah", "Makkah", "Jeddah"],
+        "total_visits": 1
+    }
+]
