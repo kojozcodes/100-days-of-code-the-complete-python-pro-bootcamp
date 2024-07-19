8bb12113aa092e43293741706372592ed808175b
Hamza Ahmed <kojozcodes@gmail.com>
2024-07-19 23:09:11 +0100

    add content that shows how dictionaries work and how they can be modified

--- Day 9 - Dictionaries,  Nesting and the Secret Auction/lesson-93.py
+++ Day 9 - Dictionaries,  Nesting and the Secret Auction/lesson-93.py
@@ -2,3 +2,18 @@ programming_dictionary = {
     "Bug": "An error in a program that prevents the program from running as expected.", 
     "Function": "A piece of code that you can easily call over and over again."
 }
+
+#Retrieving items from dictionary
+print(programming_dictionary["Function"])
+
+#Adding new items to dictionary
+programming_dictionary["Loop"] = "The action of doing something over and over again." #"Lopp" is the key, "The action of doing something over and over again." is the value.
+
+#create a new dictionary
+empty_dictionary = {}
+
+#wipe an existing dictionary
+programming_dictionary = {}
+
+#edit/override an item in a dictionary
+programming_dictionary["Bug"] = "This is how to edit an item in a dictionary"
