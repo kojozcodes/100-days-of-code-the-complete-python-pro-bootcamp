36004712cc2606582f3d811113de40db43f8c053
Hamza Ahmed <kojozcodes@gmail.com>
2024-07-21 00:03:49 +0100

    add a comment and docstring to the format_name function

--- Day 10 - Functions with Outputs/lesson-101.py
+++ Day 10 - Functions with Outputs/lesson-101.py
@@ -1,8 +1,10 @@
 #Functions with Outputs
 
 def format_name(fname, lname):
+    """Take the first name and last name and format it to return the title case version of the name."""
+
     if fname == "" or lname == "":
-        return "You did not provide vaild inputs"
+        return "You did not provide vaild inputs" #return as an early exit
     formated_f_name = fname.title() #.title() method returns an output by default, so we capture the output with a variable.
     formated_l_name = lname.title()
     return f"{formated_f_name} {formated_l_name}"
