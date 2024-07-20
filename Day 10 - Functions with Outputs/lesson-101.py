35a69e6ef1438480af3dae5486e466a400e24346
Hamza Ahmed <kojozcodes@gmail.com>
2024-07-20 23:26:59 +0100

    add logic to the function and another return staement

--- Day 10 - Functions with Outputs/lesson-101.py
+++ Day 10 - Functions with Outputs/lesson-101.py
@@ -1,9 +1,14 @@
 #Functions with Outputs
 
 def format_name(fname, lname):
+    if fname == "" or lname == "":
+        return "You did not provide vaild inputs"
     formated_f_name = fname.title() #.title() method returns an output by default, so we capture the output with a variable.
     formated_l_name = lname.title()
     return f"{formated_f_name} {formated_l_name}"
 
-formated_name = format_name("HAMza", "ahmed")
+first_name = input("What is your first name?: ")
+last_name = input("What is your last name?: ")
+
+formated_name = format_name(first_name, last_name)
 print(formated_name)
