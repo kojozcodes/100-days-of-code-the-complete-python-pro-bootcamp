63d4b378c87ca19158c224ec3c465cd50d772eb8
Hamza Ahmed <kojozcodes@gmail.com>
2024-07-21 21:50:32 +0100

    implement recursion, so users start new calculation if they want to.

--- Day 10 - Functions with Outputs/project-calculator.py
+++ Day 10 - Functions with Outputs/project-calculator.py
@@ -18,21 +18,25 @@ operations = {
     "/": divide
 }
 
+def calculator():
 
-num1 = int(input("What's the first number?: "))
-for operation in operations:
-    print(operation)
+    num1 = int(input("What's the first number?: "))
+    for operation in operations:
+        print(operation)
 
-should_continue = True
-while should_continue:
-    operation_symbol = input("Pick an operation from the line above: ")
-    num2 = int(input("What's the second number?: "))
-    function_calcultion = operations[operation_symbol]
-    answer = function_calcultion(num1, num2)
+    should_continue = True
+    while should_continue:
+        operation_symbol = input("Pick an operation from the line above: ")
+        num2 = int(input("What's the second number?: "))
+        function_calcultion = operations[operation_symbol]
+        answer = function_calcultion(num1, num2)
 
-    print(f"{num1} {operation_symbol} {num2} = {answer}")
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ").lower() == 'y':
-        num1 = answer
-    else:
-        should_continue = False
+        if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ").lower() == 'y':
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
+
+calculator()
