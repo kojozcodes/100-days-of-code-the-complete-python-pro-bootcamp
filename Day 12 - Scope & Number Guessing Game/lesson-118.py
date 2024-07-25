95f2c079aa4d07a3f08da3831408c0fb43912158
Hamza Ahmed <kojozcodes@gmail.com>
2024-07-25 22:48:40 +0100

    show that there isn't block scope in python

--- Day 12 - Scope & Number Guessing Game/lesson-118.py
+++ Day 12 - Scope & Number Guessing Game/lesson-118.py
@@ -1,9 +1,9 @@
-#Scope & Number Guessing Game
+# Scope & Number Guessing Game
 
-#Global scope
+# Global scope
 player_health = 10 #Available everywhere in the code
 
-#Local Scope
+# Local Scope
 def drink_potion():
     potion_strength = 2    # This variable is only available to this function
     print(potion_strength)
@@ -11,3 +11,12 @@ def drink_potion():
 
 print(player_health) # It will print
 print(potion_strength) #Error, It can't be accessed
+
+# There is no Block Scope in Python
+
+game_level = 3
+enemies = ["Skeleton", "Zombie", "Alien"]
+if game_level < 5:
+    new_enemy = enemies[0]    # Block level variable
+
+print(new_enemy)    # It will print
