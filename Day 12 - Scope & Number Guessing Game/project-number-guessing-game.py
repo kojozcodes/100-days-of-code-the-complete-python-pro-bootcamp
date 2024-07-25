9c81b7e9f8f92c90a592c21daed55454fc3cadcb
Hamza Ahmed <kojozcodes@gmail.com>
2024-07-26 00:17:20 +0100

    choose between difficulty, either easy or hard

--- Day 12 - Scope & Number Guessing Game/project-number-guessing-game.py
+++ Day 12 - Scope & Number Guessing Game/project-number-guessing-game.py
@@ -5,3 +5,11 @@ print("Welcome to the Number Guessing Game")
 print("I'm thinking of a number between 1 and 100")
 
 number_to_guess = random.randint(1, 100)
+print(number_to_guess)
+
+difficulty = input("Choose a difficulty. Type 'easy' or 'hard: ").lower()
+
+if difficulty == 'easy':
+    lives = 10
+else:
+    lives = 5
