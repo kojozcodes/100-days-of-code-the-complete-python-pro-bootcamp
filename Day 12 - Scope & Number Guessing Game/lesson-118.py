d8e0f330e52abeb194c7fa8876910b6343d67b07
Hamza Ahmed <kojozcodes@gmail.com>
2024-07-25 23:29:30 +0100

    show Global constants

--- Day 12 - Scope & Number Guessing Game/lesson-118.py
+++ Day 12 - Scope & Number Guessing Game/lesson-118.py
@@ -33,3 +33,8 @@ def increase_enemies():
 
 enemies = increase_enemies() # This method is better (by returning from the function and modifying outside the function)
 print(f"enemies outside function: {enemies}")
+
+# Global Constants
+PI = 3.142    # The naming convention in Python is to use UPPERCASE for Constants
+URL = "https://www.google.com"
+X_HANDLE = "@kojozcodess"
