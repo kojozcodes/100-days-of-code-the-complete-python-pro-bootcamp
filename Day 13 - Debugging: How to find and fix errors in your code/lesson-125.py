54213caecb00ac15ebc1e7cbcaa7846c66ceb3c3
Hamza Ahmed <kojozcodes@gmail.com>
2024-07-26 23:08:32 +0100

    fix the first problem

--- Day 13 - Debugging: How to find and fix errors in your code/lesson-125.py
+++ Day 13 - Debugging: How to find and fix errors in your code/lesson-125.py
@@ -2,12 +2,12 @@
 
 ############DEBUGGING#####################
 
-# # Describe Problem
-# def my_function():
-#   for i in range(1, 20):
-#     if i == 20:
-#       print("You got it")
-# my_function()
+# Describe Problem
+def my_function():
+  for i in range(1, 21):
+    if i == 20:
+      print("You got it")
+my_function()
 
 # # Reproduce the Bug
 # from random import randint
