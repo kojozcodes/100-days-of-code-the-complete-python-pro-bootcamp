4c369f5a9b86e49aab14a7ea3190a2fd5a31115b
Hamza Ahmed <kojozcodes@gmail.com>
2024-07-26 01:40:16 +0100

    change ascii art and bug fix

--- Day 12 - Scope & Number Guessing Game/project-number-guessing-game.py
+++ Day 12 - Scope & Number Guessing Game/project-number-guessing-game.py
@@ -15,7 +15,6 @@ print("Welcome to the Number Guessing Game")
 print("I'm thinking of a number between 1 and 100")
 
 number_to_guess = random.randint(1, 100)
-print(number_to_guess)
 
 difficulty = input("Choose a difficulty. Type 'easy' or 'hard: ").lower()
 
