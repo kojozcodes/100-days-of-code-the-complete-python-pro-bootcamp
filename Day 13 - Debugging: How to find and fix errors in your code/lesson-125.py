12d75ac2b9809fe62008012d5a69d2365e61e32e
Hamza Ahmed <kojozcodes@gmail.com>
2024-07-26 23:15:16 +0100

    fix second bug

--- Day 13 - Debugging: How to find and fix errors in your code/lesson-125.py
+++ Day 13 - Debugging: How to find and fix errors in your code/lesson-125.py
@@ -9,11 +9,11 @@ def my_function():
       print("You got it")
 my_function()
 
-# # Reproduce the Bug
-# from random import randint
-# dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
-# dice_num = randint(1, 6)
-# print(dice_imgs[dice_num])
+# Reproduce the Bug
+from random import randint
+dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+dice_num = randint(0,  len(dice_imgs) - 1)
+print(dice_imgs[dice_num])
 
 # # Play Computer
 # year = int(input("What's your year of birth?"))
