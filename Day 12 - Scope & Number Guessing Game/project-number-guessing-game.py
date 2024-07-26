b2a034a203b62f3a6b813a6747908db1dc7828d4
Hamza Ahmed <kojozcodes@gmail.com>
2024-07-26 01:01:29 +0100

    add the logic of the game and complete the project

--- Day 12 - Scope & Number Guessing Game/project-number-guessing-game.py
+++ Day 12 - Scope & Number Guessing Game/project-number-guessing-game.py
@@ -1,6 +1,13 @@
 import random
 from art import logo
 
+def check_wrong_guess(guess, number):
+    if guess > number:
+        return "Too High."
+    else:
+        return "Too Low."
+        
+print(logo)
 print("Welcome to the Number Guessing Game")
 print("I'm thinking of a number between 1 and 100")
 
@@ -13,3 +20,23 @@ if difficulty == 'easy':
     lives = 10
 else:
     lives = 5
+
+is_game_over = False
+
+while lives > 0 and not is_game_over:
+    print(f"You have {lives} attempts to guess the number.")
+    guess = input("Make a guess:    ")
+    if guess == "":
+        guess = 0
+    else:
+        guess = int(guess)
+
+    if guess == number_to_guess:
+        print(f"You got it! The answer was {number_to_guess}")
+        is_game_over = True
+    else:
+        print(check_wrong_guess(guess, number_to_guess))
+        lives -= 1
+
+if lives == 0:
+    print("You've run out of guesses, you lose.")
