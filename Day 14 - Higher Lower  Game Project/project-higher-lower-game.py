058c10598d8d28aa4296a1337e54a2d540edb845
Hamza Ahmed <kojozcodes@gmail.com>
2024-07-30 00:53:18 +0100

    complete project - refactor code and make game loop if answer is correct

--- Day 14 - Higher Lower  Game Project/project-higher-lower-game.py
+++ Day 14 - Higher Lower  Game Project/project-higher-lower-game.py
@@ -1,48 +1,47 @@
 import random
 from game_data import data
 from art import logo, vs
-print(logo)
 
-#Assign Celeb to variables function
-data_a = random.choice(data)
-data_b = random.choice(data)
-
-while data_b == data_a:
-    data_b = random.choice(data)
-
-#Show first celeb details
-print(f"Compare A: {data_a['name']}, a {data_a['description']}, from {data_a['country']}")
-
-#Vs Logo
-print(vs)
-
-#Show second celeb details
-print(f"Compare B: {data_b['name']}, a {data_b['description']}, from {data_b['country']}")
-
-#If correct, increment current score, assign the second celeb details as the first now then loop
-print(data_a)
-print(data_b)
-
-choice = input("Who has more followers? Type 'A' or 'B': ").upper()
 count = 0
-if choice == 'A':
-    if data_a['follower_count'] > data_b['follower_count']:
+#Assign Celeb to variables function
+def assign():
+    return random.choice(data)
+
+data_a = assign()
+data_b = assign()
+
+game_over = False
+
+while not game_over:
+    while data_b == data_a:
+        data_b = assign()
+
+    print(logo)
+
+    if count > 0:
+        print(f"You're right! Current score: {count}.")
+    #Show first celeb details
+    print(f"Compare A: {data_a['name']}, a {data_a['description']}, from {data_a['country']}")
+
+    #Vs Logo
+    print(vs)
+
+    #Show second celeb details
+    print(f"Compare B: {data_b['name']}, a {data_b['description']}, from {data_b['country']}")
+
+    #If correct, increment current score, assign the second celeb details as the first now then loop
+    # print(data_a)
+    # print(data_b)
+
+    choice = input("Who has more followers? Type 'A' or 'B': ").upper()
+    if (choice == 'A' and data_a['follower_count'] > data_b['follower_count']) or (choice == 'B' and data_b['follower_count'] > data_a['follower_count']):
         count += 1
-        print(f"You're  right! Current score: {count}.")
+        # Update data_a to be the winner and get a new data_b
         data_a = data_b
-        data_b = random.choice(data)
+        data_b = assign()
     else:
         print(f"Sorry, that's wrong. Final score: {count}")
-else:
-    if data_b['follower_count'] > data_a['follower_count']:
-        count += 1
-        print(f"You're  right! Current score: {count}.")
-        data_a = data_b
-        data_b = random.choice(data)
-    else:
-        print(f"Sorry, that's wrong. Final score: {count}")
+        game_over = True
 
-print(data_a)
-print(data_b)
-
-#If not, print you're wrong - End program
+    # print(data_a)
+    # print(data_b)
