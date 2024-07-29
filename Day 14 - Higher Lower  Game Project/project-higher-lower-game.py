fcf5cb14279eaf994ff0bcef617712ff4ae8f802
Hamza Ahmed <kojozcodes@gmail.com>
2024-07-29 23:06:37 +0100

    start over and print out data A and data B:

--- Day 14 - Higher Lower  Game Project/project-higher-lower-game.py
+++ Day 14 - Higher Lower  Game Project/project-higher-lower-game.py
@@ -4,30 +4,20 @@ from art import logo, vs
 print(logo)
 
 #Assign Celeb to variables function
-#Takes celeb at random
-def assign():
-    return random.choice(data)
+data_a = random.choice(data)
+data_b = random.choice(data)
 
-#compare two celebs function
-def compare(a, b):
-    choice = input('A' or 'B')
-    if choice == A:
-        if A > B:
-            print("A")
-        else:
-            print("B")
-    elif choice == "B":
-        if B > A:
-            print("B")
-        else:
-            print("A")
+while data_b == data_a:
+    data_b = random.choice(data)
 
 #Show first celeb details
+print(f"Compare A: {data_a['name']}, a {data_a['description']}, from {data_a['country']}")
 
 #Vs Logo
 print(vs)
 
 #Show second celeb details
+print(f"Compare B: {data_b['name']}, a {data_b['description']}, from {data_b['country']}")
 
 #If correct, increment current score, assign the second celeb details as the first now then loop
 
