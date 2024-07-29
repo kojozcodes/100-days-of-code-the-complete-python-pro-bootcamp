5ea69ac2d3f976c3f80605eedb7537627c35d2f9
Hamza Ahmed <kojozcodes@gmail.com>
2024-07-29 23:33:00 +0100

    write the basic logic of the game

--- Day 14 - Higher Lower  Game Project/project-higher-lower-game.py
+++ Day 14 - Higher Lower  Game Project/project-higher-lower-game.py
@@ -20,5 +20,29 @@ print(vs)
 print(f"Compare B: {data_b['name']}, a {data_b['description']}, from {data_b['country']}")
 
 #If correct, increment current score, assign the second celeb details as the first now then loop
+print(data_a)
+print(data_b)
+
+choice = input("Who has more followers? Type 'A' or 'B': ").upper()
+count = 0
+if choice == 'A':
+    if data_a['follower_count'] > data_b['follower_count']:
+        count += 1
+        print(f"You're  right! Current score: {count}.")
+        data_a = data_b
+        data_b = random.choice(data)
+    else:
+        print(f"Sorry, that's wrong. Final score: {count}")
+else:
+    if data_b['follower_count'] > data_a['follower_count']:
+        count += 1
+        print(f"You're  right! Current score: {count}.")
+        data_a = data_b
+        data_b = random.choice(data)
+    else:
+        print(f"Sorry, that's wrong. Final score: {count}")
+
+print(data_a)
+print(data_b)
 
 #If not, print you're wrong - End program
