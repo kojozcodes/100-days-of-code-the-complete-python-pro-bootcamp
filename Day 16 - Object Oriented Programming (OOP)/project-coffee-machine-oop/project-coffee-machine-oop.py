81e1829ff441141d2b0c8c2dfbdc1c29a94973bb
Hamza Ahmed - Kojoz <kojozcodes@gmail.com>
2024-08-13 18:42:28 +0100

    check if resource is sufficient

--- Day 16 - Object Oriented Programming (OOP)/project-coffee-machine-oop/project-coffee-machine-oop.py
+++ Day 16 - Object Oriented Programming (OOP)/project-coffee-machine-oop/project-coffee-machine-oop.py
@@ -15,11 +15,15 @@ is_on = True
 while is_on:
     choice = input(f"What would you like to order? ({coffee_menu.get_items()}): ").lower()
 
-    if choice == espresso.name or choice == latte.name or choice == cappuccino.name:
-        print(choice)
+    if choice == "off":
+        print("Goodbye!")
+        is_on = False
     elif choice == "report":
         coffee_machine.report()
         money_machine.report()
-    elif choice == "off":
-        print("Goodbye!")
-        is_on = False
+    else:
+        for coffee_index in range(len(coffee_menu.menu)):
+            coffee_type = coffee_menu.menu[coffee_index]
+            if choice == coffee_type.name:
+                if coffee_machine.is_resource_sufficient(coffee_type):
+                    print(f"can make {coffee_type.name}")
