f3e10b808b001afe527ee4417fea7ba6cfc175c9
Hamza Ahmed - Kojoz <kojozcodes@gmail.com>
2024-08-13 18:22:59 +0100

    create objects for Menu, CoffeMaker, MoneyMachine, and MenuItem classes

--- Day 16 - Object Oriented Programming (OOP)/project-coffee-machine-oop/project-coffee-machine-oop.py
+++ Day 16 - Object Oriented Programming (OOP)/project-coffee-machine-oop/project-coffee-machine-oop.py
@@ -1,3 +1,11 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+coffee_menu = Menu()
+coffee_machine = CoffeeMaker()
+money_machine = MoneyMachine()
+
+espresso = coffee_menu.find_drink("espresso")
+latte = coffee_menu.find_drink("latte")
+cappuccino = coffee_menu.find_drink("cappuccino")
