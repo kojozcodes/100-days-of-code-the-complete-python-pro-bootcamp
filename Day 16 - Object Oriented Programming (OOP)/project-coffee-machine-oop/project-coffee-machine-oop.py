851e50cbc1ee798e55ad74a7783eb22aa345328e
Hamza Ahmed - Kojoz <kojozcodes@gmail.com>
2024-08-13 18:28:24 +0100

    add a while loop to make the coffee machine ask the choice of coffee after a user uses it unless it is turned off

--- Day 16 - Object Oriented Programming (OOP)/project-coffee-machine-oop/project-coffee-machine-oop.py
+++ Day 16 - Object Oriented Programming (OOP)/project-coffee-machine-oop/project-coffee-machine-oop.py
@@ -10,13 +10,14 @@ espresso = coffee_menu.find_drink("espresso")
 latte = coffee_menu.find_drink("latte")
 cappuccino = coffee_menu.find_drink("cappuccino")
 
+is_on = True
 
+while is_on:
+    choice = input(f"What would you like to order? ({coffee_menu.get_items()}): ").lower()
 
-choice = input(f"What would you like to order? ({coffee_menu.get_items()}): ").lower()
-
-if choice == "off":
-    print("Goodbye!")
-    is_on = False
-elif choice == "report":
-    coffee_machine.report()
-    money_machine.report()
+    if choice == "off":
+        print("Goodbye!")
+        is_on = False
+    elif choice == "report":
+        coffee_machine.report()
+        money_machine.report()
