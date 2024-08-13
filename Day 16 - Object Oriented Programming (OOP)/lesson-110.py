af3bfb2753f3d8aa102ba3d264bab7ab826a388a
Hamza Ahmed - Kojoz <kojozcodes@gmail.com>
2024-08-13 01:57:09 +0100

    add content to table and format table

--- Day 16 - Object Oriented Programming (OOP)/lesson-110.py
+++ Day 16 - Object Oriented Programming (OOP)/lesson-110.py
@@ -15,3 +15,9 @@ my_screen.exitonclick()
 
 from prettytable import PrettyTable
 table = PrettyTable()
+
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = "l"
+
+print(table)
