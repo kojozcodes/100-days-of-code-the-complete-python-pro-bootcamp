f848eb57879e110640e158a30fa93e9fb31e6297
Hamza Ahmed - Kojoz <kojozcodes@gmail.com>
2024-08-13 18:30:42 +0100

    check if user choice of coffee is correct

--- Day 16 - Object Oriented Programming (OOP)/project-coffee-machine-oop/project-coffee-machine-oop.py
+++ Day 16 - Object Oriented Programming (OOP)/project-coffee-machine-oop/project-coffee-machine-oop.py
@@ -15,9 +15,11 @@ is_on = True
 while is_on:
     choice = input(f"What would you like to order? ({coffee_menu.get_items()}): ").lower()
 
-    if choice == "off":
-        print("Goodbye!")
-        is_on = False
+    if choice == espresso.name or choice == latte.name or choice == cappuccino.name:
+        print(choice)
     elif choice == "report":
         coffee_machine.report()
         money_machine.report()
+    elif choice == "off":
+        print("Goodbye!")
+        is_on = False
