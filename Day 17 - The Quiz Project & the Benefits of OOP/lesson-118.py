0d6d6a7d48a3737073a2124375a57beff3c13bd1
Hamza Ahmed - Kojoz <kojozcodes@gmail.com>
2024-08-14 19:35:20 +0100

    add follow method to the user class

--- Day 17 - The Quiz Project & the Benefits of OOP/lesson-118.py
+++ Day 17 - The Quiz Project & the Benefits of OOP/lesson-118.py
@@ -5,9 +5,20 @@ class User:
         self.id = user_id
         self.username = username
         self.followers = 0
+        self.following = 0
+
+
+    def follow(self, user):
+        self.following += 1
+        user.followers += 1
 
 
 user_1 = User("001", "hamza")
 user_2 = User("002", "ahmed")
 
+user_1.follow(user_2)
+
 print(user_1.followers)
+print(user_1.following)
+print(user_2.followers)
+print(user_2.following)
