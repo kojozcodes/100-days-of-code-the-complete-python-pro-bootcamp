7d6f84e63ff83bae9b702d930ce09f913f6c7b7a
Hamza Ahmed - Kojoz <kojozcodes@gmail.com>
2024-08-17 02:49:04 +0100

    draw a square with turtle

---
+++ Day 18 - Turtle & the Graphical User Interface (GUI)/lesson-128.py
@@ -0,0 +1,23 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("red")
+# timmy.forward(100)
+# timmy.backward(200)
+# timmy.right(90)
+# timmy.left(180)
+# timmy.setheading(0)
+
+######## Challenge 1 - Draw a Square ############
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+timmy.right(90)
+
+screen = Screen()
+screen.exitonclick()
