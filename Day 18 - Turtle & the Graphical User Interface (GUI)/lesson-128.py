a3b01a0ca2f6fa011dbf412e0f1ab85abb5b3094
Hamza Ahmed - Kojoz <kojozcodes@gmail.com>
2024-08-17 03:15:54 +0100

    draw a dashed line

--- Day 18 - Turtle & the Graphical User Interface (GUI)/lesson-128.py
+++ Day 18 - Turtle & the Graphical User Interface (GUI)/lesson-128.py
@@ -10,14 +10,27 @@ timmy.color("red")
 # timmy.setheading(0)
 
 ######## Challenge 1 - Draw a Square ############
-timmy.forward(100)
-timmy.right(90)
-timmy.forward(100)
-timmy.right(90)
-timmy.forward(100)
-timmy.right(90)
-timmy.forward(100)
-timmy.right(90)
+# timmy.forward(100)
+# timmy.right(90)
+# timmy.forward(100)
+# timmy.right(90)
+# timmy.forward(100)
+# timmy.right(90)
+# timmy.forward(100)
+# timmy.right(90)
+#
+# #shorter version
+# for side in range(4):
+#     timmy.forward(100)
+#     timmy.left(90)
+
+
+########### Challenge 2 - Draw a Dashed Line ########
+for line in range(15):
+    timmy.forward(10)
+    timmy.penup()
+    timmy.forward(10)
+    timmy.pendown()
 
 screen = Screen()
 screen.exitonclick()
